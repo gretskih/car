aff3cd7051126dfc6b60901c2a88264f6f4f7adb
AGretskikh <gretskih@mail.ru>
2023-12-11 16:44:42 +0300

    2. HQL

--- src/main/java/ru/job4j/car/model/User.java
+++ src/main/java/ru/job4j/car/model/User.java
@@ -2,12 +2,16 @@ package ru.job4j.car.model;
 
 import lombok.*;
 
-@Getter
-@Setter
+import javax.persistence.*;
+
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
-@ToString
+@Entity
+@Table(name = "auto_user")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String login;
     private String password;
